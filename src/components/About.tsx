444e6f5340fdcb5bdbce5b82ca7e39c59fb6b842
KingTingTheGreat <jting@bu.edu>
2024-01-13 01:56:20 -0500

    uses <Link/> tag instead of <a> tag. underline styling with tailwind css rather than <u> tag.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,28 +1,29 @@
 import React from "react";
+import Link from "next/link";
 
 export default function About() {
 	return (
 		<div id="about">
 			<div className="text-center p-10 text-xl">
 				Use this website to track your{" "}
-				<u>
-					<a href="https://pikminbloom.com/">Pikmin Bloom</a>
-				</u>{" "}
+				<Link href="https://pikminbloom.com/" className="underline">
+					Pikmin Bloom
+				</Link>{" "}
 				decor pikmin collection progress! Simply click the pikmin you would like to mark as collected and view
 				some of your stats after! Please note, this website is not made for commercial purposes, and all images
 				used orignate from Pikmin Bloom and are not owned by me. Pikmin and icon images were found on{" "}
-				<u>
-					<a href="https://www.pikminwiki.com/Decor_Pikmin">Pikipedia</a>
-				</u>{" "}
+				<Link href="https://www.pikminwiki.com/Decor_Pikmin" className="underline">
+					Pikipedia
+				</Link>{" "}
 				and this{" "}
-				<u>
-					<a href="https://github.com/zviznemte/PikminBloomAssets">repository</a>
-				</u>
+				<Link href="https://github.com/zviznemte/PikminBloomAssets" className="underline">
+					repository
+				</Link>
 				. This website is not affiliated with Nintendo nor Niantic, and all credits for the original images and
 				icons go to them. See the GitHub repository for this website{" "}
-				<u>
-					<a href="https://github.com/ridanaeeem/pikmin-progress-tracker">here</a>
-				</u>
+				<Link href="https://github.com/ridanaeeem/pikmin-progress-tracker" className="underline">
+					here
+				</Link>
 				.
 			</div>
 		</div>
