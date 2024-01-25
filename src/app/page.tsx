441476afecd13528c9d2d35d793c404574331452
Jeffrey <jting@bu.edu>
2024-01-25 17:05:17 -0500

    syntax correction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import NavMenu from "@/components/NavMenu";
 import AllPikmin from "@/components/AllPikmin";
-import About from "@/components/About"
+import About from "@/components/About";
 
 export default function Home() {
 	const [showMenu, setShowMenu] = useState("hidden");
@@ -22,7 +22,7 @@ export default function Home() {
 					≡
 				</div>
 				<div className={showMenu}>
-					<NavMenu showMenu={showMenu} setShowMenu={setShowMenu}/>
+					<NavMenu showMenu={showMenu} setShowMenu={setShowMenu} />
 				</div>
 				<div id="title" className="">
 					<a href="/">PIKMIN PROGRESS TRACKER</a>
