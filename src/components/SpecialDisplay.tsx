4ff9157f75e51d6c5c21da1331a1441881c7d278
ridanaeeem <ridan@bu.edu>
2024-07-06 11:30:01 -0500

    reorganize navmenu, split icon display and special display into their own separate components

---
+++ src/components/SpecialDisplay.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import React from "react";
+
+export default function SpecialDisplay() {
+	return (
+		<div id="specialDisplay">
+			<Link href="#mariohat">Mario Hat</Link>
+			<Link href="#lunarnewyearornament:red">Lunar New Year Ornament</Link>
+			<Link href="#whitechesspiece">Chess Piece</Link>
+			<Link href="#fingerboard">Fingerboard</Link>
+			<Link href="#flowercard">Flower Card</Link>
+			<Link href="#jack-o-lantern">Jack-o-lantern</Link>
+			<Link href="#halloweentreat">Halloween Treat</Link>
+			<Link href="#firstanniversarysnack">First Anniversary Snack</Link>
+			<Link href="#koppaitespacesuit">Koppaite Space Suit</Link>
+			<Link href="#mitten">Mitten</Link>
+			<Link href="#2023glasses">2023 Glasses</Link>
+			<Link href="#2024glasses">2024 Glasses</Link>
+			<Link href="#valentinesticker">Valentine Sticker</Link>
+			<Link href="#reversevalentine'sdaysticker">Reverse Valentine&apos;s Day Sticker</Link>
+			<Link href="#presentsticker">Present Sticker</Link>
+			<Link href="#easteregg">Easter Egg</Link>
+			<Link href="#bunnyegg">Bunny Egg</Link>
+			<Link href="#sneakerkeychain">Sneaker Keychain</Link>
+			<Link href="#pikmin4spaceships">Pikmin 4 Spaceships</Link>
+			<Link href="#mahjongtiles">Mahjong Tiles</Link>
+			<Link href="#icecream">Ice Cream</Link>
+			<Link href="#puzzle:2021fallmemories">Puzzle: 2021 Fall Memories</Link>
+			<Link href="#puzzle:2022summermemories">Puzzle: 2022 Summer Memories</Link>
+			<Link href="#springsticker">Spring Sticker</Link>
+			<Link href="#wintersticker">Winter Sticker</Link>
+			<Link href="#playingcard">Playing Card</Link>
+			<Link href="#cheese">Cheese</Link>
+			<Link href="#coral">Coral</Link>
+		</div>
+	);
+}
